64d9195c6a07e6fdbd7d87eec5e267e8dcb07bcb
Ethan Chan <ethan.ccj@gmail.com>
2021-12-15 22:29:40 +0800

    updated logic

--- index.js
+++ index.js
@@ -1,94 +1,212 @@
 require('dotenv').config() ;
-const { ALCHEMY_API_KEY, CONTRACT_ADDRESS, WALLET } = process.env;
-
-const https = require('https');
+let mysql = require('mysql');
 const { ethers } = require('ethers');
+const Web3 = require('web3');
 const ContractABI = require('./contracts/RKPRIM.json');
 
-let lastBlock = 1;
-let contract = null;
+const { 
+  ALCHEMY_API_KEY, 
+  ALCHEMY_WEB_SOCKET, 
+  LISTEN_ADDRESS,
+  MYSQL_HOST,
+  MYSQL_USERNAME,
+  MYSQL_PASSWORD,
+  MYSQL_DB,
+  MYSQL_TABLE,
+  TRANSACTION_CONFIRMATION_MIN,
+  TRANSACTION_CONFIRMATION_RETRY_INTERVAL
+} = process.env;
 
-console.log(`starting scan_pusher wallet: ${WALLET}`)
+let connection;
 
-const httpsGet = async (url) => {
-  return new Promise((resolve, reject) => {
-    https.get(url, res => {
-      let data = [];
-      console.log('Status Code:', res.statusCode);
-
-      res.on('data', chunk => {
-        data.push(chunk);
-      });
-
-      res.on('end', () => {
-        console.log('Response ended: ');
-        console.log(JSON.parse(Buffer.concat(data).toString()));
-        resolve(JSON.parse(Buffer.concat(data).toString()))
-      });
-
-    }).on('error', err => {
-      console.log('Error: ', err.message);
-      reject(err);
-    });
-  });
-}
-
-const getBalance = () => {
-  // todo - replace with alchemy to get that contract balance
+function validateTransaction(trx) {
+  const toValid = trx.to !== null
+  if (!toValid) return false
   
+  const walletToValid = trx.to.toLowerCase() === LISTEN_ADDRESS.toLowerCase()
+
+  return toValid && walletToValid;
 }
 
-const getTransactions = () => {
-  const result = httpsGet(`https://api.polygonscan.com/api?module=account&action=txlist&address=${WALLET}&startblock=${lastBlock}&endblock=99999999&sort=asc&apikey=${POLYGON_API_KEY}`)
-  
-  result.then(
-    (response) => {
-      console.log(response.result);
-    }, () => {
-      console.log('error')
-    });
-}
+/**
+ * Calls the smart contract splitter
+ */
+const callSmartContract = (contractAddress) => {
+  if (contractAddress === '' || contractAddress === null) {
+    console.log('error, smart contract address empty');
+    return;
+  }
 
-const getLastBlockAccess = () => {
-  
-}
-
-const updateLastBlockAccess = () => {
-
-}
-
-const getArtistNFT = (tx) => {
-  // get from tx extra data, meta somewhere
-  // artist id, each artist splittng contract is different
-  // search by public key, token id
-}
-
-// when nft is listed for sale, non minted wallet
-const callSplitterContract = () => {
-  // need contractABI and contract address, 
-  // parameters: company B address (godkey D72), amount
-}
-
-// run this every 23:59 UTC
-// get transactions from last block access onwards
-// iterate for each transaction, if artist id / nft contract id is in database
-
-const main = async () => {
   const customHttpProvider = new ethers.providers.JsonRpcProvider(ALCHEMY_API_KEY);
-  
   const signer = customHttpProvider.getSigner();
   const contract = new ethers.Contract(
-    CONTRACT_ADDRESS,
+    contractAddress,
     ContractABI,
     signer
   );
+  contract.distribute();
+}
 
-  const balance = ethers.utils.formatEther(await customHttpProvider.getBalance(CONTRACT_ADDRESS))
+/**
+ * 
+ * @param {*} result 
+ * 
+ * Actions on confirmed transaction
+ */
+const actionOnConfirmedTransaction = (result) => {
+  // get NFT public key from transaction
+  const nftId = ''
 
-  console.log('contract balance', balance);
+  if (nftId === '' || nftId === null) {
+    console.log('error NFT Id not found');
+    return;
+  }
 
-  // listen for events
+  // query MySQL DB for contract address and artist address
+  if (connection == undefined) {
+    console.log('No DB connection available')
+    return;
+  }
+
+  connection.query('SELECT smartContract, EthKey FROM ' + MYSQL_TABLE + ' WHERE TokenID = ?', 
+    [nftId],
+    (err, results, fields) => {
+      if (err) {
+        console.log(err.message)
+      }
+      
+      if (results.length === 1) {
+        const ethKey = results[0].EthKey; // not sure if i need this
+        const smartContract = results[0].smartContract;
+        callSmartContract(smartContract);
+      }
+    }
+  );
+
+}
+
+async function getConfirmations(txHash) {
+  try {
+    // Instantiate web3 with HttpProvider
+    const web3 = new Web3(ALCHEMY_API_KEY);
+
+    // Get transaction details
+    const trx = await web3.eth.getTransaction(txHash);
+
+    // Get current block number
+    const currentBlock = await web3.eth.getBlockNumber();
+
+    // When transaction is unconfirmed, its block number is null.
+    // In this case we return 0 as number of confirmations
+    return trx.blockNumber === null ? 0 : currentBlock - trx.blockNumber
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+async function getTransactionReceipt(txHash) {
+  try {
+    // Instantiate web3 with HttpProvider
+    const web3 = new Web3(ALCHEMY_API_KEY);
+
+    const receipt = await web3.eth.getTransactionReceipt(txHash);
+    
+    return receipt.status;
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function confirmTransaction(txHash, confirmations = TRANSACTION_CONFIRMATION_MIN) {
+  setTimeout(async () => {
+    
+    /*
+    // Get current number of confirmations and compare it with sought-for value
+    const trxConfirmations = await getConfirmations(txHash)
+    
+    console.log('Transaction with hash ' + txHash + ' has ' + trxConfirmations + ' confirmation(s)')
+
+    if (trxConfirmations >= confirmations) {
+      // Handle confirmation event according to your business logic
+      
+      console.log('Transaction with hash ' + txHash + ' has been successfully confirmed')
+      
+      actionOnConfirmedTransaction(txHash);
+
+      return;
+    }
+    */
+
+    // get the transaction receipt, if status is TRUE, transaction is completed
+    const status = await getTransactionReceipt(txHash);
+    console.log('Transaction with hash ' + txHash + ' has ' + status + ' status')
+
+    if (status) {
+      // Handle confirmation event according to your business logic
+      
+      console.log('Transaction with hash ' + txHash + ' has been successfully confirmed')
+      
+      actionOnConfirmedTransaction(txHash);
+
+      return;
+    }
+
+    // Recursive call
+    return confirmTransaction(txHash, confirmations)
+  }, TRANSACTION_CONFIRMATION_RETRY_INTERVAL * 1000)
+}
+
+const main = async () => {
+  connection = mysql.createConnection({
+    host: MYSQL_HOST,
+    user: MYSQL_USERNAME,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DB
+  });
+
+  connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
   
+    console.log('Connected to the MySQL server.', MYSQL_HOST);
+
+    const web3 = new Web3(ALCHEMY_WEB_SOCKET);
+    web3.eth.getBlockNumber().then(console.log);  // -> 7946893
+
+    const web3Http = new Web3(ALCHEMY_API_KEY);
+
+    var subscription = web3.eth.subscribe('pendingTransactions');
+
+    subscription.subscribe(async (error, txHash) => {
+      if (error) { 
+        console.log('error', error) 
+        return;
+      }
+
+      const trx = await web3Http.eth.getTransaction(txHash);
+
+      const valid = validateTransaction(trx);
+      // If transaction is not valid, simply return
+      if (!valid) return
+
+      console.log('Transaction hash is: ' + txHash + '\n');
+
+      confirmTransaction(txHash);
+
+      subscription.unsubscribe();
+    })
+
+  });
+
 }
 
 main();
+
+console.log('press any key to exit')
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.on('data', process.exit.bind(process, 0));
